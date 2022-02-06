0c7378f6f41f2bf7cf70a23890a9b2d9c4709193
Mamdouh <mamdouhsobhy923@gmail.com>
2022-02-06 18:46:07 +0200

    Edvora Paint

--- app/src/main/java/com/example/edvora/MainActivity.kt
+++ app/src/main/java/com/example/edvora/MainActivity.kt
@@ -17,6 +17,7 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding?.root)
 
+
         initialize()
         addListenerOnView()
 
